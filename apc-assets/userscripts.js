25bbd8b14549cda4f6364977cfa1d43fe35db613
lzy1960 <lzy196055@gmail.com>
2024-05-03 13:44:57 +0800

    feat: vscode右键菜单hover过渡动画

--- apc-assets/userscripts.js
+++ apc-assets/userscripts.js
@@ -63,6 +63,10 @@ function ContextmenuStylesheet() {
     background: #00000000;
     backdrop-filter: blur(8px);
 }
+.monaco-menu .action-label,
+.monaco-menu .action-menu-item  {
+    transition: all 0.2s ease-in-out;
+}
     `)
     return sheet
 }
